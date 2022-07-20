339cace1b361052151ee91d1ddc32274626cfcb7
Prebero <reberoplacide@gmail.com>
2022-07-20 10:44:52 +0200

    ft-user-register-#182711347

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegistrationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/welcome',[\App\Http\Controllers\Welcome\MessageController::class,'index']);
+Route::post('/register',[RegistrationController::class,'register']);
+
